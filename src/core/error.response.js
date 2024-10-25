c3bad51aa13a1c6fb3ab4153b8f8837de5d4df5d
quy_le_van <quy_le_van@bita.jp>
2024-10-25 15:00:44 +0700

    feat: login

--- src/core/error.response.js
+++ src/core/error.response.js
@@ -10,9 +10,11 @@ const ReasonStatusCode = {
   CONFLICT: 'Conflict error',
 }
 
+const { StatusCodes, ReasonPhrases } = require("../utils/httpStatusCode")
+
 class ErrorResponse extends Error {
   constructor(message, status) {
-    super(message)
+    super(message) 
     this.status = status
   }
 }
@@ -30,7 +32,14 @@ class BadRequestError extends ErrorResponse {
   }
 }
 
+class AuthFailureError extends ErrorResponse {
+  constructor(message = ReasonPhrases.UNAUTHORIZED, statusCode = StatusCodes.UNAUTHORIZED) {
+    super(message, statusCode)
+  }
+}
+
 module.exports = {
   BadRequestError,
-  ConflictRequestError
+  ConflictRequestError,
+  AuthFailureError
 }
--- src/services/access.service.js
+++ src/services/access.service.js
@@ -6,7 +6,8 @@ const shopModel = require("../models/shop.model");
 const KeyTokenService = require("./keyToken.service");
 const { createTokenPair } = require("../auth/authUtils");
 const { getInfoData } = require("../utils");
-const { BadRequestError } = require("../core/error.response");
+const { BadRequestError, AuthFailureError } = require("../core/error.response");
+const { findByEmail } = require("./shop.service");
 
 const RoleShop = {
   SHOP: "SHOP",
@@ -16,6 +17,32 @@ const RoleShop = {
 };
 
 class AccessService {
+  static login = async ({ email, password, refreshToken = null }) => {
+    const foundShop = await findByEmail({ email });
+    if (!foundShop) throw new BadRequestError("Shop not registered");
+
+    const match = bcrypt.compare(password, foundShop.password);
+    if (!match) throw new AuthFailureError("Authentication error");
+
+    const privateKey = crypto.randomBytes(64).toString("hex");
+    const publicKey = crypto.randomBytes(64).toString("hex");
+
+    const { _id: userId } = foundShop
+    const tokens = await createTokenPair({ userId, email }, publicKey, privateKey);
+
+    await KeyTokenService.createKeyToken({
+      refreshToken: tokens.refreshToken,
+      privateKey,
+      publicKey,
+      userId
+    })
+
+    return {
+      shop: getInfoData({ fields: ["_id", "name", "email"], object: foundShop }),
+      tokens,
+    };
+  };
+
   static signUp = async ({ name, email, password }) => {
     const holderShop = await shopModel.findOne({ email }).lean();
     if (holderShop) {
@@ -31,36 +58,25 @@ class AccessService {
     });
 
     if (newShop) {
-      const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
-        modulusLength: 4096,
-        publicKeyEncoding: {
-          type: "pkcs1",
-          format: "pem",
-        },
-        privateKeyEncoding: {
-          type: "pkcs1",
-          format: "pem",
-        },
-      });
+      const privateKey = crypto.randomBytes(64).toString("hex");
+      const publicKey = crypto.randomBytes(64).toString("hex");
 
-      const publicKeyString = await KeyTokenService.createKeyToken({
+
+      const keyStore = await KeyTokenService.createKeyToken({
         userId: newShop._id,
         publicKey,
+        privateKey,
       });
 
-      if (!publicKeyString) {
+      if (!keyStore) {
         throw new BadRequestError("Error: publicKeyString error");
       }
 
-      const publicKeyObject = crypto.createPublicKey(publicKeyString);
       const tokens = await createTokenPair({ userId: newShop._id, email }, publicKey, privateKey);
 
       return {
-        code: 201,
-        metadata: {
-          shop: getInfoData({ fields: ["_id", "name", "email"], object: newShop }),
-          tokens,
-        },
+        shop: getInfoData({ fields: ["_id", "name", "email"], object: newShop }),
+        tokens,
       };
     }
 
